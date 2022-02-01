91a1ab278fb0bafe20f97cb352ca04783b11ef1a
Dmitry <dbezruchenko@ex.ua>
2022-02-01 02:17:15 +0200

    Added relative routing

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
       <ThemeProvider theme={theme}>
         <SnackbarProvider>
           <App />
